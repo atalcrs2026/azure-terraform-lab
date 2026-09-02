2f84b8fd02b2789ab81ba7fec2fdee6d4ae2d10f
atalcrs2026 <a.t.alcaraz89@gmail.com>
2026-09-02 22:57:47 +0800

    Configure Terraform remote state backend

--- main.tf
+++ main.tf
@@ -1,4 +1,25 @@
 resource "azurerm_resource_group" "lab" {
-  name     = "rg-terraform-lab"
-  location = "Southeast Asia"
+  name     = var.resource_group_name
+  location = var.location
+}
+
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "lab" {
+  name                     = "tfstorage${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.lab.name
+  location                 = azurerm_resource_group.lab.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "lab" {
+  name                  = "lab-container"
+  storage_account_id    = azurerm_storage_account.lab.id
+  container_access_type = "private"
 }
--- provider.tf
+++ provider.tf
@@ -4,6 +4,18 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-lab"
+    storage_account_name = "tfstoragedmoki1"
+    container_name       = "lab-container"
+    key                  = "terraform.tfstate"
   }
 }
 
